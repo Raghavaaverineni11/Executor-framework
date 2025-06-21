dd7f93d94b1253b655158e63bb49f99cbb3b7e82
raghava averineni <raghavaoffc@gmail.com>
2025-06-21 13:21:26 -0500

    classes

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,26 @@
+
+import java.util.concurrent.*;
+
 public class Hello {
+
+    public static void main(String[] args) throws Exception
+    {
+
+        ExecutorService e=Executors.newFixedThreadPool(3);
+
+            Future<?> s = e.submit(() -> System.out.println(Thread.currentThread().getName()));
+            System.out.println(s);
+
+
+
+        System.out.println(Thread.currentThread().getName());
+        e.shutdown();
+        System.out.println(e.isShutdown());
+
+
+
+
+
+    }
+
 }
